9d0575f462f34b382ed92d6f4c672e04d7404790
Minh Tri <you@example.com>
2022-06-21 13:58:29 +0700

    Header and Footer

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>
 );
 
