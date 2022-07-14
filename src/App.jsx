9928cffa01c5bdc394a8a03071a76f3783eb939f
Minh Tri <you@example.com>
2022-07-14 13:42:06 +0700

    React Animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
-import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import Main from './components/MainComponent';
+import './App.css';
 
 const store = ConfigureStore();
 
 class App extends Component {
+  
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }  
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES
-        };
-    }
-
-
-    render() {
-        return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    <div className="App">
-                        <Main />
-                    </div>
-                </BrowserRouter>
-            </Provider>
-        );
-    }
 }
 
 export default App;
