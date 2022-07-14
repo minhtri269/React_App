297072607508f7ab230bb2dfa5d4cb7742d7d2d3
Minh Tri <you@example.com>
2022-07-14 13:24:12 +0700

    Fetch from Server

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -1,89 +1,3 @@
-// import React from 'react';
-// import {
-// 	Card, CardImg, CardText, CardBody,
-// 	CardTitle, Breadcrumb, BreadcrumbItem
-// } from 'reactstrap';
-// import { Link } from 'react-router-dom';
-
-
-// function RenderDish({ dish }) {
-
-// 	return (
-// 		<div className="col-12 col-md-5 m-1">
-// 			<Card>
-// 				<CardImg top src={dish.image} alt={dish.name} />
-// 				<CardBody>
-// 					<CardTitle>{dish.name}</CardTitle>
-// 					<CardText>{dish.description}</CardText>
-// 				</CardBody>
-// 			</Card>
-// 		</div>
-// 	);
-
-// }
-
-// function RenderComments({ comments }) {
-
-// 	if (comments != null) {
-// 		return (
-// 			<div className="col-12 col-md-5 m-1">
-// 				<h4>Comments</h4>
-// 				<ul className="list-unstyled">
-// 					{comments.map((comment) => {
-// 						return (
-// 							<li key={comment.id}>
-// 								<p>{comment.comment}</p>
-// 								<p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-// 							</li>
-// 						);
-// 					})}
-// 				</ul>
-// 			</div>
-// 		);
-// 	}
-// 	else {
-// 		return (
-// 			<div></div>
-// 		);
-// 	}
-
-// }
-
-// const DishDetail = (props) => {
-
-// 	if (props.dish != null) {
-// 		return (
-// 			<div className="container">
-// 				<div className="row">
-// 					<Breadcrumb>
-
-// 						<BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-// 						<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-// 					</Breadcrumb>
-// 					<div className="col-12">
-// 						<h3>{props.dish.name}</h3>
-// 						<hr />
-// 					</div>
-// 				</div>
-// 				<div className="row">
-// 					<RenderDish dish={props.dish} />
-// 					<RenderComments comments={props.comments} />
-// 				</div>
-// 			</div>
-// 		);
-// 	}
-// 	else {
-// 		return (
-// 			<div></div>
-// 		);
-// 	}
-
-// }
-
-// export default DishDetail;
-
-
-/* eslint-disable react/jsx-pascal-case */
 /* eslint-disable react/jsx-pascal-case */
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, 
@@ -92,6 +6,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -199,7 +114,8 @@ function RenderDish({dish}) {
    return (
       <div className="col-12 col-md-5 m-1">
          <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
+            {/* <CardImg top src={dish.image} alt={dish.name} /> */}
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                <CardTitle><h5 class="mb-2 text-info">{dish.name}</h5></CardTitle>
                <CardText>{dish.description}</CardText>
