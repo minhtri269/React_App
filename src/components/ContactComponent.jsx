4904f6dfb8c238090679270b58fa5a3e36540119
Minh Tri <you@example.com>
2022-07-15 14:30:09 +0700

    lab8

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem,
         Button, Row, Col, Label } from 'reactstrap';
-import { Control, Form, Errors } from 'react-redux-form';
+import {  Control, LocalForm, Form, Errors, actions } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 const required = (val) => val && val.length;
@@ -19,8 +19,9 @@ class Contact extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        // console.log('Current State is: ' + JSON.stringify(values));
+        // alert('Current State is: ' + JSON.stringify(values));
+        this.props.postFeedback( values.firstname, values.lastname, values.telnum, values.email, values.agree, values.contactType, values.message );
         this.props.resetFeedbackForm();
         // event.preventDefault();
     }
@@ -71,7 +72,7 @@ class Contact extends Component {
                     </div>
 					<div className="col-12 col-md-9">
                         {/* <LocalForm onSubmit={(values) => this.handleSubmit(values)}> */}
-                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)} resetOnSubmit={true}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
